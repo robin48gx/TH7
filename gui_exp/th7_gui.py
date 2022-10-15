df08420aeef9efe813c498f3c3d9cf3db4253f79
Robin P. Clark <r.clark@energytechnologycontrol.com>
2022-10-15 16:01:44 +0100

    OK first page of the GUI done. Need to learn how to make the configure window come up now

--- gui_exp/th7_gui.py
+++ gui_exp/th7_gui.py
@@ -1,20 +1,15 @@
 from guizero import App, Combo, Text, CheckBox, PushButton, info, Box, Picture
 
 #def do_configure(channel):
-def do_configure(channel=8):
-    print ("configure", channel)
-    if channel <= 7:
-       info("configure", channel)
+def do_configure(args):
+    print (args)
+    # print ("configure", channel)
+    # if channel <= 7:
+       # info("configure", channel)
 
-# grid [x,y] where y goes down
-
-def do_pb(argyarg):
-    info("does this work box?", argyarg)
 
 
 app = App(title="TH7 Thermocouple Pi-HAT", width=600, height=600)
-# why does this stupid button call the callback on init arggghhh
-wb = PushButton(app, command=do_configure(0), text="wb...", align="right")
 
 title_box = Box(app, width="fill", align="top", border=True, height=100)
 title = Text(title_box, text="TH7 Vcc=5.11 Vref=4.096 status=OK ", align="bottom")
@@ -45,18 +40,24 @@ temp = [1,2,3,4,5,6,7,8]
 status = [1,2,3,4,5,6,7,8]
 th_choice = ["K","K","K","K","K","K","K","R"]
 
+
+
+conf[1] = PushButton(th7_box, command=lambda : do_configure(1) , text="config"+str(1), grid=[2,ydown+0], align="left")
+conf[2] = PushButton(th7_box, command=lambda : do_configure(2) , text="config"+str(2), grid=[2,ydown+1], align="left")
+conf[3] = PushButton(th7_box, command=lambda : do_configure(3) , text="config"+str(3), grid=[2,ydown+2], align="left")
+conf[4] = PushButton(th7_box, command=lambda : do_configure(4) , text="config"+str(4), grid=[2,ydown+3], align="left")
+conf[5] = PushButton(th7_box, command=lambda : do_configure(5) , text="config"+str(5), grid=[2,ydown+4], align="left")
+conf[6] = PushButton(th7_box, command=lambda : do_configure(6) , text="config"+str(6), grid=[2,ydown+5], align="left")
+conf[7] = PushButton(th7_box, command=lambda : do_configure(7) , text="config"+str(7), grid=[2,ydown+6], align="left")
+
 for i in range (1,8):
   channel[i] = Text(th7_box, text=str(i), grid=[0,ydown], align="left")
   th_choice[i] = Combo(th7_box, options=["K", "J", "R", "T"], grid=[1,ydown], align="left")
-  conf[i] = PushButton(th7_box, command=do_configure(), text="config...", grid=[2,ydown], align="left")
-  #conf[i].args=i
   temp[i] = Text(th7_box, text="137.4", grid=[3,ydown], align="left")
   status[i] = Text(th7_box, text="OK", grid=[4,ydown], align="left")
   ydown += 1
 
 
-pb = PushButton(app, command=do_pb(i), text="another button", align="left")
-my_cat = Picture(bot_box, image="dog.png")
 
 app.display()
 
